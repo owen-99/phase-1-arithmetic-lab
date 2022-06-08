26ca363b1c2dbc715a87157a3a4df8f831488bb6
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-08 07:23:03 +0300

    commit tree dae7780535718e1e9877036b37a359076a04b360

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 5 + 1);
+
+let num3 = 31
+let num4 = 9
+let mod = num3 % num4;
+
+let max = Math.max(12,14,16,18,20)
